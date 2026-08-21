c7df5ac77daa485f60f396adb16f400f5a712525
nishad1711 <nishadnmehta@gmail.com>
2026-08-21 13:22:30 +0530

    34A

---
+++ Reconnaissance2_34A.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Reconnaissance2_34A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = Integer.MAX_VALUE;
+        int ans1 = 0;
+        int ans2 = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            int diff = Math.abs(arr[i] - arr[i + 1]);
+
+            if (diff < min) {
+                min = diff;
+                ans1 = i;
+                ans2 = i + 1;
+            }
+        }
+
+        int diff = Math.abs(arr[n - 1] - arr[0]);
+
+        if (diff < min) {
+            ans1 = n - 1;
+            ans2 = 0;
+        }
+
+        System.out.println(ans1 + " " + ans2);
+    }
+}
--- Soldier_and_Bananas_545A.java
+++ Soldier_and_Bananas_545A.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Soldier_and_Bananas{
+public class Soldier_and_Bananas_545A{
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
         int k=sc.nextInt();
