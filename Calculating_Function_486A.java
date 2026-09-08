6d5fb6d95e70c0ef6b453891cec4d6b7454588f8
nishad1711 <nishadnmehta@gmail.com>
2026-09-08 11:07:42 +0530

    update in 486A

--- Calculating_Function_486A.java
+++ Calculating_Function_486A.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
 
 public class Calculating_Function_486A {
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        int n= sc.nextInt();
-        int sum=0;
-        for(int i=1;i<=n;i++){
-            if(i%2==0){
-                sum+=i;
-            }
-            else{
-                sum-=i;
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        if (n % 2 == 0) {
+            System.out.println(n / 2);
+        } else {
+            System.out.println(-(n + 1) / 2);
         }
-        System.out.println(sum);
     }
 }
